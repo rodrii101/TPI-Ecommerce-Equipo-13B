04aad0787389fd675094b431840b1b0e264c2719
darioruiz80 <dario80cursos@gmail.com>
2026-06-02 22:16:17 -0300

    Acceso a Datos

--- negocioEcommerce/AccesoDatos.cs
+++ negocioEcommerce/AccesoDatos.cs
@@ -1,12 +1,64 @@
-﻿using System;
+﻿using dominioEcommerce;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using dominioEcommerce;
 namespace negocioEcommerce
 {
     public class AccesoDatos
     {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+        public SqlDataReader Lector
+        {
+            get { return lector; }
+        }
+        public AccesoDatos()
+        {
+            conexion = new SqlConnection("server=localhost; database=CATALOGO_P3_DB; user id=sa; password=BaseDatos#2;");
+            comando = new SqlCommand();
+        }
+        public void ejecutarLectura()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void ejecutarAccion()
+        {
+            comando.Connection = conexion;
+            try
+            {
+                conexion.Open();
+                comando.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        public void cerrarConexion()
+        {
+            if (lector != null)
+            {
+                conexion.Close();
+            }
+            conexion.Close();
+        }
+        public void setearProcedimiento(string procedimiento)
+        {
+            comando.CommandType = System.Data.CommandType.StoredProcedure;
+            comando.CommandText = procedimiento;
+        }
     }
 }
